c614ad75cf124c6a860c1d55319c74740b56f9b4
Furkan Ay <furkan-ay@outlook.com>
2022-08-04 12:42:27 +0200

    Configured eslint, prettier and husky

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'import/prefer-default-export': [0],
+  },
+};
