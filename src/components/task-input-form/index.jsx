5cc075a2db63128d1d1d5c43bcf4c020bc0668d7
Furkan Ay <furkan-ay@outlook.com>
2022-08-04 19:16:08 +0200

    Created TaskInputForm component

---
+++ src/components/task-input-form/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styles from './style.module.scss';
+
+function TaskInput({ onSubmit }) {
+  const [taskValue, setTaskValue] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (taskValue && onSubmit) {
+      const newTask = {
+        id: Date.now(),
+        value: taskValue,
+        isDone: false,
+      };
+
+      onSubmit(newTask);
+      setTaskValue('');
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setTaskValue(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit} className={styles.inputForm}>
+      <input
+        type="text"
+        placeholder="Enter your task!"
+        value={taskValue}
+        onChange={handleInputChange}
+      />
+    </form>
+  );
+}
+
+TaskInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export { TaskInput };
